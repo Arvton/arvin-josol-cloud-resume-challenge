763e179bc7b1e8e62d1dfc8eea05e1292622e835
Arvin Josol <arvin.josol@live.com>
2025-02-02 17:49:30 -0800

    add storage account with static website and custom domain

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,3 +18,22 @@ resource "azurerm_resource_group" "crc-test-terraform" {
     environment = "test"
   }
 }
+
+resource "azurerm_storage_account" "sa-crc-test" {
+  name                     = "sacrctest"
+  resource_group_name      = azurerm_resource_group.crc-test-terraform.name
+  location                 = azurerm_resource_group.crc-test-terraform.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  static_website {
+    index_document     = "index.html"
+    error_404_document = "404.html"
+  }
+  custom_domain {
+    name = "terraform.arvinjosol.ca"
+  }
+  tags = {
+    environment = "test"
+  }
+}
